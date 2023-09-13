58f3f999c131c0521a122e75d13d498ccf4698ae
Sakis T. <106918030+AthTop@users.noreply.github.com>
2023-09-13 19:14:00 +0300

    Implement computer's choice

--- js/rock-paper-scissors.js
+++ js/rock-paper-scissors.js
@@ -1,7 +1,12 @@
+// since hand options are fixed, we'll use a global array for them
+const HAND = ['rock', 'paper', 'scissors'];
+
 // computer needs to choose a hand 
 // we can do this with a function
 function getComputerChoice() {
-
+    // use random to generate a random number based on the lenght of our array and floor to round it
+    computerChoice = HAND[Math.floor(Math.random() * HAND.length)];
+    return computerChoice;
 }
 
 // ask player for their hand
