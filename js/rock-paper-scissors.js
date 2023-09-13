e9c8dff0f80a9047d9d872ddeffa770e8f6d33df
Sakis T. <106918030+AthTop@users.noreply.github.com>
2023-09-13 19:03:26 +0300

    Write pseudocode and initialize functions

---
+++ js/rock-paper-scissors.js
@@ -0,0 +1,19 @@
+// computer needs to choose a hand 
+// we can do this with a function
+function getComputerChoice() {
+
+}
+
+// ask player for their hand
+// a function to ask the user for their choice
+function playerSelection() {
+
+}
+
+// compare the two hands 
+function playRound(getComputerChoice, playerSelection) {
+
+}
+
+// give out a result 
+console.log(playRound);
