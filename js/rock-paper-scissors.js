c03280c8a232c492723f611bba9d856ea4719cbb
Sakis T. <106918030+AthTop@users.noreply.github.com>
2023-09-13 19:55:29 +0300

    implement player's selection

--- js/rock-paper-scissors.js
+++ js/rock-paper-scissors.js
@@ -5,15 +5,23 @@ const HAND = ['rock', 'paper', 'scissors'];
 // we can do this with a function
 function getComputerChoice() {
     // use random to generate a random number based on the lenght of our array and floor to round it
-    computerChoice = HAND[Math.floor(Math.random() * HAND.length)];
+    let computerChoice = HAND[Math.floor(Math.random() * HAND.length)];
     return computerChoice;
 }
 
 // ask player for their hand
 // a function to ask the user for their choice
 function playerSelection() {
-
-}
+    // ask user for hand
+    let playerChoice = prompt('What hand will you throw? Rock, paper or scissors?', '');
+    
+    // make sure the user inputs a valid hand, else call the function again until they do
+    if (! HAND.includes(playerChoice.toLowerCase)) {
+        console.log('Please enter a valid hand; Choices are rock, paper, scissors')
+        playerChoice = playerSelection();
+    }
+    return playerChoice;
+}   
 
 // compare the two hands 
 function playRound(getComputerChoice, playerSelection) {
