186defc3da3e7e5b95830f5658267eeecb235a78
Sakis T. <106918030+AthTop@users.noreply.github.com>
2023-09-13 19:55:56 +0300

    Set helper functions to help playRound

--- js/rock-paper-scissors.js
+++ js/rock-paper-scissors.js
@@ -14,19 +14,51 @@ function getComputerChoice() {
 function playerSelection() {
     // ask user for hand
     let playerChoice = prompt('What hand will you throw? Rock, paper or scissors?', '');
-    
+
     // make sure the user inputs a valid hand, else call the function again until they do
-    if (! HAND.includes(playerChoice.toLowerCase)) {
+    if (playerChoice === null) {
+        console.log('You cancelled');
+    } else if (! HAND.includes(playerChoice.toLowerCase())) {
         console.log('Please enter a valid hand; Choices are rock, paper, scissors')
         playerChoice = playerSelection();
-    }
+    } 
     return playerChoice;
 }   
 
 // compare the two hands 
-function playRound(getComputerChoice, playerSelection) {
-
+function playRound(computerChoice, playerChoice) {
+    if (isTie(computerChoice, playerChoice)) {
+        console.log('It\'s a tie');
+    } else {
+        let winner = compareHands(computerChoice, playerChoice);
+        if (winner) {
+            console.log('Computer wins with: ' + computerChoice + ' vs ' + playerChoice);
+        }else {
+            console.log('Player wins with: ' + playerChoice + ' vs ' + computerChoice);
+        } 
+   }
 }
 
+// helper function to decide winning hand
+function compareHands(hand1, hand2) {
+    if ((hand1 === 'rock' && hand2 === 'scissors') ||
+        (hand1 === 'scissors' && hand2 === 'paper') ||
+        (hand1 === 'paper' && hand2 === 'rock')) {
+        return 0;
+    } else {
+        return 1;
+    }
+}
+
+// helper function for tie
+function isTie(hand1, hand2) {
+    if (hand1 === hand2) {
+        return true;
+    }
+    return false;
+}
+
+
+
 // give out a result 
-console.log(playRound);
+console.log(playRound(getComputerChoice(), playerSelection()));
